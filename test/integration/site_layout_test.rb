f3cb04bc0d65d1c37db334f852c737b662741369
koichi0617 <koichi0617@i.softbank.jp>
2020-08-11 00:42:39 +0900

    Add password reset

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,6 +12,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "title", full_title("アカウント登録")
   end
 end
