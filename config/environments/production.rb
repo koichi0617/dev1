abdd8b10bb4612be0a146f5c0b89ec7317a2a727
koichi0617 <koichi0617@i.softbank.jp>
2020-08-10 18:14:21 +0900

    last change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://quiet-oasis-47889.herokuapp.com/'
+  host = 'yamasakikoichi.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
