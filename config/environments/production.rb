389075493d591cb22cd54592786cef838657a581
koichi0617 <koichi0617@i.softbank.jp>
2020-08-14 00:16:23 +0900

    mail setting master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://yagile-inlet-26178.herokuapp.com/'
+  host = 'https://agile-inlet-26178.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
