8ca4838450adab562963c2155c2a10b0ac2c2b3a
koichi0617 <koichi0617@i.softbank.jp>
2020-09-05 15:22:02 +0900

    chenge production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'herokuapp.com',
     :enable_starttls_auto => true
   }
