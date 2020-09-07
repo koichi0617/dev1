bd9b21b17ab3665957c78a4d75dd798e9e443da2
koichi0617 <koichi0617@i.softbank.jp>
2020-09-07 17:27:41 +0900

    linebot change

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,18 +12,15 @@ class LinebotController < ApplicationController
   end
 
   def callback
-    logger.debug('/callback')
     body = request.body.read
-    logger.debug(body)
-    logger.debug('XXXXXXXXXXXX')
+
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    logger.debug('YYYYYYYYYYYY')
     unless client.validate_signature(body, signature)
       head :bad_request
     end
-    logger.debug('AAAAAAAAAAAA')
+
     events = client.parse_events_from(body)
-    logger.debug('BBBBBBBBBBBB')
+
     events.each { |event|
       case event
       when Line::Bot::Event::Message
@@ -33,11 +30,10 @@ class LinebotController < ApplicationController
             type: 'text',
             text: event.message['text']
           }
-          logger.debug(message)
         end
       end
     }
-    logger.debug('CCCCCCCCCCC')
+
     head :ok
   end
 end
