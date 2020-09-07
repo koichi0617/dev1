1326768156662e73034e60d4fef81420b8caab47
koichi0617 <koichi0617@i.softbank.jp>
2020-09-07 18:04:11 +0900

    change callback

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -13,15 +13,13 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :bad_request
+      error 400 do 'Bad Request' end
     end
-
     events = client.parse_events_from(body)
 
-    events.each { |event|
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -32,8 +30,8 @@ class LinebotController < ApplicationController
           }
         end
       end
-    }
-
+      client.reply_message(event['replyToken'], message)
+    end
     head :ok
   end
 end
