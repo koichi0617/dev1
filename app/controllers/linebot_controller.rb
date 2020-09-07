2887351429b30fb9724ab5a1e1d70c280e2b86b6
koichi0617 <koichi0617@i.softbank.jp>
2020-09-07 15:37:51 +0900

    some changes

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -7,7 +7,7 @@ class LinebotController < ApplicationController
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_token = ENV["LINE_ACCESS_TOKEN"]
     }
   end
 
