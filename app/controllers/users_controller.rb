23de599aa087e704cc5b2c7fbd51421a026ab66e
koichi0617 <koichi0617@i.softbank.jp>
2020-09-13 18:22:03 +0900

    uri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -107,16 +107,18 @@ class UsersController < ApplicationController
     decoded_id_token = JWT.decode(id_token, nil, false, { algorithm: 'HS256' })
     logger.error("==================")
     logger.error("decoded_id_token = #{decoded_id_token}")
-    de_id_token = JSON.parse(decoded_id_token[0], quirks_mode: true)
     logger.error("==================")
-    logger.error("de_id_token = #{de_id_token}")
+    logger.error("decoded_id_token[0] = #{decoded_id_token[0]}")
+    # de_id_token = JSON.parse(decoded_id_token[0], quirks_mode: true)
+    # logger.error("==================")
+    # logger.error("de_id_token = #{de_id_token}")
     # expected_nonce = decoded_id_token.get('nonce')
     # if nonce != decoded_id_token.get('nonce')
     #   raise RuntimeError('invalid nonce')
     # end
 
     #usersテーブルに値を格納
-    @user.open_id = de_id_token['sub']
+    @user.open_id = decoded_id_token[0]['sub'].first
     logger.error("==================")
     logger.error("open_id = #{@user.open_id}")
   end
